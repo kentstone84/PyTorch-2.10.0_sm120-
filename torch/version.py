96d3195520cd40fd1a98626f9638fc4c7d5dc3ed
Kent Stone <69402437+kentstone84@users.noreply.github.com>
2025-10-29 01:56:17 -0500

    Delete torch directory

--- torch/version.py
+++
@@ -1,9 +0,0 @@
-from typing import Optional
-
-__all__ = ['__version__', 'debug', 'cuda', 'git_version', 'hip', 'xpu']
-__version__ = '2.10.0a0+gite67e3d9'
-debug = False
-cuda: Optional[str] = '13.0'
-git_version = 'e67e3d95f3ab863c8b44a6b16ce9f25d40b0d517'
-hip: Optional[str] = None
-xpu: Optional[str] = None
